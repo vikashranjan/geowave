b56fb4b0eff0cce4df456a5eeafab9411cb8c4c7
Rich Fecher <rfecher@gmail.com>
2016-04-05 12:51:25 -0400

    broke apart accumulo setup

--- deploy/emr/4/gdelt-example/ingest-and-kde-gdelt.sh
+++ deploy/emr/4/gdelt-example/ingest-and-kde-gdelt.sh
@@ -2,16 +2,6 @@
 echo "Ingesting GeoWave sample data and running kernel density estimate..."
 source geowave-env.sh
 
-## configure accumulo
-cat <<EOF | accumulo shell -u root -p secret -e "createuser geowave"
-geowave
-geowave
-EOF
-accumulo shell -u root -p secret -e "createnamespace geowave"
-accumulo shell -u root -p secret -e "grant NameSpace.CREATE_TABLE -ns geowave -u geowave"
-accumulo shell -u root -p secret -e "config -s general.vfs.context.classpath.geowave=hdfs://${HOSTNAME}:${HDFS_PORT}/accumulo/classpath/geowave/${GEOWAVE_VERSION}-apache/[^.].*.jar"
-accumulo shell -u root -p secret -e "config -ns geowave -s table.classpath.context=geowave"
-
 mkdir $STAGING_DIR/gdelt;cd $STAGING_DIR/gdelt
 # just grab whateve data matches the time regex
 wget http://data.gdeltproject.org/events/md5sums
